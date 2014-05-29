57d09c47e5149fb2311e7f1a295467b5d28068ef
Matt Zabriskie <mzabriskie@gmail.com>
2014-05-29 10:51:03 -0400

    basic impl

---
+++ index.js
@@ -0,0 +1,16 @@
+var drone = require('ar-drone'),
+  client = drone.createClient();
+
+client.takeoff();
+
+client
+  .after(5000, function() {
+    this.clockwise(0.5);
+  })
+  .after(3000, function() {
+    this.animate('flipLeft', 15);
+  })
+  .after(1000, function() {
+    this.stop();
+    this.land();
+  });
